7319616b01a5568340718b1d6de54fad7bd388fc
Gabe Guralnick <gnguralnick@gmail.com>
2023-04-17 23:35:19 -0400

    fix typo

--- training-scripts/textual-inversion/train_textual_inversion.py
+++ training-scripts/textual-inversion/train_textual_inversion.py
@@ -22,10 +22,11 @@ DEFAULT_MODEL_NAME = "runwayml/stable-diffusion-v1-5"
 
 
 def run_training(target_images_dir, model_output_dir, model_name, placeholder_token, initializer_token,
-                 hyperparameters):
+                 hyperparameters, resume_checkpoint=None):
     """Run the textual inversion training."""
-    os.system(f"accelerate launch textual-inversion.py \
+    command = f"accelerate launch textual_inversion.py \
         --pretrained_model_name_or_path={model_name} \
+        --only_save_embeds \
         --train_data_dir={target_images_dir} \
         --learnable_property=\"object\" \
         --placeholder_token=\"{placeholder_token}\" --initializer_token=\"{initializer_token}\" \
@@ -38,7 +39,10 @@ def run_training(target_images_dir, model_output_dir, model_name, placeholder_to
         --lr_warmup_steps={hyperparameters['lr_warmup_steps']} \
         --enable_xformers_memory_efficient_attention \
         --seed={hyperparameters['seed']} \
-        --output_dir={model_output_dir}")
+        --output_dir={model_output_dir}"
+    if resume_checkpoint:
+        command += f" --resume_from_checkpoint={resume_checkpoint}"
+    os.system(command)
     # return whether the training was successful
     return os.path.exists(f"{model_output_dir}/checkpoint-{hyperparameters['max_train_steps']}")
 
@@ -58,7 +62,7 @@ def run_inference(model_output_path, generated_images_dir, placeholder_token, hy
 
 
 def main(target_images_dir, initializer_token="object", model_output_dir=None, model_name=DEFAULT_MODEL_NAME,
-         placeholder_token="<*>", generated_images_dir=None, train=True, train_log="training.log"):
+         placeholder_token="<*>", generated_images_dir=None, train=True, train_log="training.log", resume_checkpoint=None):
     target_images_dir_name = args.target_images_dir.split("/")[-1]
 
     if model_output_dir is None:
@@ -68,22 +72,30 @@ def main(target_images_dir, initializer_token="object", model_output_dir=None, m
 
     os.system(f"rm -rf {generated_images_dir}")
 
-    with open(train_log, "w") as f:
-        f.write(f"Running textual inversion training for the target images in {target_images_dir}.")
+    if not resume_checkpoint:
+        with open(train_log, "w") as f:
+            f.write(f"Running textual inversion training for the target images in {target_images_dir}.\n")
 
-    if train:
-        model_training_successful = run_training(target_images_dir, model_output_dir, model_name,
-                                                 placeholder_token, initializer_token, HYPERPARAMETERS)
+        if train:
+            model_training_successful = run_training(target_images_dir, model_output_dir, model_name,
+                                                     placeholder_token, initializer_token, HYPERPARAMETERS)
+        else:
+            model_training_successful = True
     else:
-        model_training_successful = True
+        with open(train_log, "a") as f:
+            f.write(f"Resuming training from checkpoint {resume_checkpoint}.\n")
+        model_training_successful = run_training(target_images_dir, model_output_dir, model_name,
+                                                 placeholder_token, initializer_token, HYPERPARAMETERS, resume_checkpoint)
 
     if model_training_successful:
         model_output_path = model_output_dir
     else:
+        with open(train_log, "a") as f:
+            f.write("Model training failed. Exiting.\n")
         sys.exit("Model training failed. Exiting.")
 
     with open(train_log, "a") as f:
-        f.write("Training completed successfully, beginning inference.")
+        f.write("Training completed successfully, beginning inference.\n")
     run_inference(model_output_path, generated_images_dir, placeholder_token, HYPERPARAMETERS)
 
     with open(train_log, "a") as f:
@@ -100,7 +112,8 @@ if __name__ == "__main__":
     parser.add_argument("--initializer_token", type=str, required=False, default="object")
     parser.add_argument("--generated_images_dir", type=str, required=False)
     parser.add_argument("--train", type=bool, required=False, default=True)
+    parser.add_argument("--resume_checkpoint", type=str, required=False)
     args = parser.parse_args()
 
     main(args.target_images_dir, args.initializer_token, args.model_output_dir, args.model_name, args.placeholder_token,
-         args.generated_images_dir, args.train, args.logfile)
+         args.generated_images_dir, args.train, args.logfile, args.resume_checkpoint)
