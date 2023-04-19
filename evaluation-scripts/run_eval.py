4bf85123cd6471d361a8f46f06fa7df94ed20ac9
Gabe Guralnick <gnguralnick@gmail.com>
2023-04-19 11:03:00 -0400

    fix issue in run_eval.py

--- evaluation-scripts/run_eval.py
+++ evaluation-scripts/run_eval.py
@@ -6,18 +6,25 @@ import clip_distance
 import fid
 
 
-def run_eval(method_name, checkpoint_steps):
+def run_eval(method_name, checkpoint_steps=None):
 
-    results_file = open(f"../evaluation-results/{method_name}-{checkpoint_steps}.txt", "w")
+    if checkpoint_steps is not None:
+        results_file = open(f"../evaluation-results/{method_name}-step-{checkpoint_steps}.txt", "w")
+    else:
+        results_file = open(f"../evaluation-results/{method_name}.txt", "w")
     results_file.write("prompt-type,target-name,avg-fid,avg-clip")
+    generated_dir = f"../generated-images/{method_name}/"
 
     # compute FID and CLIP scores for basic prompt
-    basic_prompt_dir = f"../generated-images/{method_name}/basic"
+
     basic_target_dir = "../target-images"
     basic_target_names = os.listdir(basic_target_dir)
     overall_fid = 0
     overall_clip = 0
     for target_name in basic_target_names:
+        basic_prompt_dir = os.path.join(generated_dir, target_name, "basic")
+        if checkpoint_steps is not None:
+            basic_prompt_dir = os.path.join(basic_prompt_dir, f"step-{checkpoint_steps}")
         target_dir = os.path.join(basic_target_dir, target_name)
         avg_fid = fid.main(target_dir, basic_prompt_dir)
         avg_clip = clip_distance.main(target_dir, basic_prompt_dir)
@@ -34,13 +41,16 @@ def run_eval(method_name, checkpoint_steps):
     overall_fid = 0
     overall_clip = 0
     for prompt in edit_prompts:
-        prompt_dir = f"../generated-images/{method_name}/{prompt}"
+
         prompt_target_dir = f"../target-complex-images/{prompt}"
         prompt_target_names = os.listdir(prompt_target_dir)
         prompt_overall_fid = 0
         prompt_overall_clip = 0
         for target_name in prompt_target_names:
             target_dir = os.path.join(prompt_target_dir, target_name)
+            prompt_dir = os.path.join(generated_dir, target_name, prompt)
+            if checkpoint_steps is not None:
+                prompt_dir = os.path.join(prompt_dir, f"step-{checkpoint_steps}")
             avg_fid = fid.main(target_dir, prompt_dir)
             avg_clip = clip_distance.main(target_dir, prompt_dir)
             results_file.write(f"{prompt},{target_name},{avg_fid},{avg_clip}")
@@ -66,7 +76,8 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Compute evaluation metrics for a given method and checkpoint step.")
     parser.add_argument("--method_name", type=str, help="Name of the method used for generating images.")
-    parser.add_argument("--checkpoint_steps", type=int, help="Number of steps used for training the model.")
+    parser.add_argument("--checkpoint_steps", type=int, required=False, default=None, help="Number of steps used for "
+                                                                                           "training the model.")
 
     args = parser.parse_args()
 
